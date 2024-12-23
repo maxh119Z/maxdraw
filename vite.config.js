f61efc5048d9823612d7a4cc1e258f660882a54e
lzhangcs <63130594+lzhangcs@users.noreply.github.com>
2024-12-23 11:44:48 -0800

    Add source files

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This must match the "outputDirectory" in vercel.json
-  },
   base: '/maxdraw/'
 })
 
